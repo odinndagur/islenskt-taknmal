95ea13a499f08e8aa83d2d93df33c8066927327b
odinndagur <odinndagur@users.noreply.github.com>
2022-11-18 16:18:32 +0000

    deploy: 8ddd43636afd05ba60126c954c5a46888ee75c30

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/islenskt-taknmal/sw.js', { scope: '/islenskt-taknmal/' })})}
